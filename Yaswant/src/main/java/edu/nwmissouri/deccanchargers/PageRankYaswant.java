d7806cd593e09deb3e164efe1565c9d58f4e8100
yaswant1 <lakkarajuyaswant@gmail.com>
2022-04-19 22:28:09 -0500

    assignment 4/19 yaswant

--- Yaswant/src/main/java/edu/nwmissouri/deccanchargers/PageRankYaswant.java
+++ Yaswant/src/main/java/edu/nwmissouri/deccanchargers/PageRankYaswant.java
@@ -17,6 +17,8 @@
  */
 package edu.nwmissouri.deccanchargers;
 
+import java.util.ArrayList;
+
 // beam-playground:
 //   name: MinimalWordCount
 //   description: An example that counts words in Shakespeare's works.
@@ -29,11 +31,14 @@ package edu.nwmissouri.deccanchargers;
 //     - Core Transforms
 
 import java.util.Arrays;
+import java.util.Collection;
+
 import org.apache.beam.sdk.Pipeline;
 import org.apache.beam.sdk.io.TextIO;
 import org.apache.beam.sdk.options.PipelineOptions;
 import org.apache.beam.sdk.options.PipelineOptionsFactory;
 import org.apache.beam.sdk.transforms.Count;
+import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
@@ -82,6 +87,29 @@ import org.apache.beam.sdk.values.PCollectionList;
  * file service.
  */
 public class PageRankYaswant {
+        static class Job1Finalizer extends DoFn<KV<String, Iterable<String>>, KV<String, RankedPage>> {
+                @ProcessElement
+                public void processElement(@Element KV<String, Iterable<String>> element,
+                                OutputReceiver<KV<String, RankedPage>> receiver) {
+                        Integer contributorVotes = 0;
+                        if (element.getValue() instanceof Collection) {
+                                contributorVotes = ((Collection<String>) element.getValue()).size();
+                        }
+                        ArrayList<VotingPage> voters = new ArrayList<VotingPage>();
+                        for (String voterName : element.getValue()) {
+                                if (!voterName.isEmpty()) {
+                                        voters.add(new VotingPage(voterName, contributorVotes));
+                                }
+                        }
+                        receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
+                }
+        }
+
+        static class Job1Mapper extends DoFn<KV<String, RankedPage>, KV<String, RankedPage>> {
+        }
+
+        static class Job1Updater extends DoFn<KV<String, Iterable<RankedPage>>, KV<String, RankedPage>> {
+        }
 
         public static void main(String[] args) {
 
