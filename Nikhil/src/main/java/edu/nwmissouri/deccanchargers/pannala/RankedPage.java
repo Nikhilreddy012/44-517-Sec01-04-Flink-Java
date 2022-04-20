b72a4f3a023a33bce2140c1d9faf8ad80c8c108e
Nikhil Reddy Pannala <nikhilreddypannala.98@gmail.com>
2022-04-19 22:54:19 -0500

    Added job1 finalizer class in page rank

--- Nikhil/src/main/java/edu/nwmissouri/deccanchargers/pannala/RankedPage.java
+++ Nikhil/src/main/java/edu/nwmissouri/deccanchargers/pannala/RankedPage.java
@@ -1,5 +1,19 @@
 package edu.nwmissouri.deccanchargers.pannala;
 
+import java.util.ArrayList;
+
 public class RankedPage {
+    String name;
+    Double rank;
+    Integer vote;
+
+    public RankedPage(String name, Double rank, Integer vote) {
+        this.name = name;s
+        this.rank = rank;
+        this.vote = vote;
+    }
+
+    public RankedPage(String key, ArrayList<VotingPage> voters) {
+    }
     
 }
