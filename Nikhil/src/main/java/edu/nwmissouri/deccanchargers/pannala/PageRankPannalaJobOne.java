4cde633583083b35ece0018ac03d7e0b667fb852
Nikhil Reddy Pannala <nikhilreddypannala.98@gmail.com>
2022-04-27 14:23:12 -0500

    Updated job 2

--- Nikhil/src/main/java/edu/nwmissouri/deccanchargers/pannala/PageRankPannalaJobOne.java
+++ Nikhil/src/main/java/edu/nwmissouri/deccanchargers/pannala/PageRankPannalaJobOne.java
@@ -13,7 +13,9 @@ import org.apache.beam.sdk.transforms.DoFn;
 import org.apache.beam.sdk.transforms.Filter;
 import org.apache.beam.sdk.transforms.FlatMapElements;
 import org.apache.beam.sdk.transforms.Flatten;
+import org.apache.beam.sdk.transforms.GroupByKey;
 import org.apache.beam.sdk.transforms.MapElements;
+import org.apache.beam.sdk.transforms.ParDo;
 import org.apache.beam.sdk.values.KV;
 import org.apache.beam.sdk.values.TypeDescriptors;
 import org.apache.beam.sdk.values.PCollection;
@@ -22,7 +24,7 @@ import org.apache.beam.sdk.values.PDone;
 import org.apache.beam.sdk.values.TypeDescriptor;
 
 public class PageRankPannalaJobOne {
-          // DEFINE DOFNS
+  // DEFINE DOFNS
   // ==================================================================
   // You can make your pipeline assembly code less verbose by defining
   // your DoFns statically out-of-line.
@@ -56,35 +58,109 @@ public class PageRankPannalaJobOne {
       receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), voters)));
     }
   }
-    public static void main(String[] args) {
-        PipelineOptions options = PipelineOptionsFactory.create();
-        Pipeline pl = Pipeline.create(options);
-        String dataFolder = "web04";
 
-        PCollection<KV<String, String>> pColctn1 = PannalaMapper01(pl, "java.md", dataFolder);
-        PCollection<KV<String, String>> pColctn2 = PannalaMapper01(pl, "go.md", dataFolder);
-        PCollection<KV<String, String>> pColctn3 = PannalaMapper01(pl, "python.md", dataFolder);
-        PCollection<KV<String, String>> pColctn4 = PannalaMapper01(pl, "README.md", dataFolder);
+  static class Job2Mapper extends DoFn<KV<String, RankedPage>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, RankedPage> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      int votes = 0;
+      ArrayList<VotingPage> voters = element.getValue().getVoterList();
+      if (voters instanceof Collection) {
+        votes = ((Collection<VotingPage>) voters).size();
+      }
+      for (VotingPage vp : voters) {
+        String pageName = vp.getVoterName();
+        double pageRank = vp.getPageRank();
+        String contributingPageName = element.getKey();
+        double contributingPageRank = element.getValue().getRank();
+        VotingPage contributor = new VotingPage(contributingPageName, votes, contributingPageRank);
+        ArrayList<VotingPage> arr = new ArrayList<>();
+        arr.add(contributor);
+        receiver.output(KV.of(vp.getVoterName(), new RankedPage(pageName, pageRank, arr)));
+      }
+    }
+  }
 
-        PCollectionList<KV<String, String>> pColctnList = PCollectionList.of(pColctn1).and(pColctn2).and(pColctn3)
-                .and(pColctn4);
-        PCollection<KV<String, String>> merList = pColctnList.apply(Flatten.<KV<String, String>>pCollections());
-        PCollection<String> merListStr = merList
-                .apply(MapElements.into(TypeDescriptors.strings()).via((mergeOut) -> mergeOut.toString()));
-        merListStr.apply(TextIO.write().to("PannalaPR"));
-        pl.run().waitUntilFinish();
+  static class Job2Updater extends DoFn<KV<String, Iterable<RankedPage>>, KV<String, RankedPage>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, Iterable<RankedPage>> element,
+        OutputReceiver<KV<String, RankedPage>> receiver) {
+      Double dampingFactor = 0.85;
+      Double updatedRank = (1 - dampingFactor);
+      ArrayList<VotingPage> newVoters = new ArrayList<>();
+      for (RankedPage rankPage : element.getValue()) {
+        if (rankPage != null) {
+          for (VotingPage votingPage : rankPage.getVoterList()) {
+            newVoters.add(votingPage);
+            updatedRank += (dampingFactor) * votingPage.getPageRank() / (double) votingPage.getContributorVotes();
+          }
+        }
+      }
+      receiver.output(KV.of(element.getKey(), new RankedPage(element.getKey(), updatedRank, newVoters)));
+
     }
 
-    public static PCollection<KV<String, String>> PannalaMapper01(Pipeline p, String filename, String dataFolder) {
-        String dataPath = dataFolder + "/" + filename;
-        PCollection<String> pColctnInp = p.apply(TextIO.read().from(dataPath));
-        PCollection<String> pColctnLinkLines = pColctnInp.apply(Filter.by((String line) -> line.startsWith("[")));
-        PCollection<String> pColctnLinks = pColctnLinkLines.apply(MapElements.into((TypeDescriptors.strings()))
-                .via((String linkLine) -> linkLine.substring(linkLine.indexOf("(") + 1, linkLine.length() - 1)));
-        PCollection<KV<String, String>> pColctnKVPairs = pColctnLinks
-                .apply(MapElements.into(TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.strings()))
-                        .via((String outputLink) -> KV.of(filename, outputLink)));
-        return pColctnKVPairs;
+  }
+
+  static class Job3 extends DoFn<KV<String, RankedPage>, KV<Double, String>> {
+    @ProcessElement
+    public void processElement(@Element KV<String, RankedPage> element,
+        OutputReceiver<KV<Double, String>> receiver) {
+      receiver.output(KV.of(element.getValue().getRank(), element.getKey()));
     }
+  }
+
+  public static void main(String[] args) {
+    PipelineOptions options = PipelineOptionsFactory.create();
+    Pipeline pl = Pipeline.create(options);
+    String dataFolder = "web04";
+
+    PCollection<KV<String, String>> pColctn1 = PannalaMapper01(pl, "java.md", dataFolder);
+    PCollection<KV<String, String>> pColctn2 = PannalaMapper01(pl, "go.md", dataFolder);
+    PCollection<KV<String, String>> pColctn3 = PannalaMapper01(pl, "python.md", dataFolder);
+    PCollection<KV<String, String>> pColctn4 = PannalaMapper01(pl, "README.md", dataFolder);
+
+    PCollectionList<KV<String, String>> pColctnList = PCollectionList.of(pColctn1).and(pColctn2).and(pColctn3)
+        .and(pColctn4);
+    PCollection<KV<String, String>> merList = pColctnList.apply(Flatten.<KV<String, String>>pCollections());
+    PCollection<KV<String, Iterable<String>>> pColGroupByKey = merList.apply(GroupByKey.create());
+    PCollection<KV<String, RankedPage>> job2in = pColGroupByKey.apply(ParDo.of(new Job1Finalizer()));
+
+    PCollection<KV<String, RankedPage>> job2out = null;
+
+    int numOfIterations = 40;
+    for (int i = 1; i <= numOfIterations; i++) {
+      PCollection<KV<String, RankedPage>> job2Mapper = job2in.apply(ParDo.of(new Job2Mapper()));
+
+      PCollection<KV<String, Iterable<RankedPage>>> job2MapperGrpByKey = job2Mapper.apply(GroupByKey.create());
+
+      job2out = job2MapperGrpByKey.apply(ParDo.of(new Job2Updater()));
+      job2in = job2out;
+    }
+
+    PCollection<String> pColctnStringLists = job2out.apply(
+        MapElements.into(
+            TypeDescriptors.strings()).via(
+                kvtoString -> kvtoString.toString()));
+    pColctnStringLists.apply(TextIO.write().to("PannalaPR"));
+
+    pl.run().waitUntilFinish();
+  }
+
+  public static PCollection<KV<String, String>> PannalaMapper01(Pipeline p, String filename, String dataFolder) {
+    PCollection<String> pColInputLines = p.apply(TextIO.read().from(dataFolder + "/" + filename));
+    PCollection<String> pColLinkLines = pColInputLines.apply(Filter.by((String line) -> line.startsWith("[")));
+    PCollection<String> pColLinks = pColLinkLines.apply(
+        MapElements.into(
+            TypeDescriptors.strings())
+            .via(
+                (String linkLine) -> linkLine.substring(linkLine.indexOf("(") + 1, linkLine.indexOf(")"))));
+
+    PCollection<KV<String, String>> pColkvs = pColLinks.apply(
+        MapElements.into(
+            TypeDescriptors.kvs(TypeDescriptors.strings(), TypeDescriptors.strings())).via(
+                outLink -> KV.of(filename, outLink)));
+    return pColkvs;
+  }
 
 }
--- Nikhil/src/main/java/edu/nwmissouri/deccanchargers/pannala/RankedPage.java
+++ Nikhil/src/main/java/edu/nwmissouri/deccanchargers/pannala/RankedPage.java
@@ -1,19 +1,46 @@
 package edu.nwmissouri.deccanchargers.pannala;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class RankedPage {
-    String name;
-    Double rank;
-    Integer vote;
-
-    public RankedPage(String name, Double rank, Integer vote) {
-        this.name = name;s
-        this.rank = rank;
-        this.vote = vote;
-    }
-
-    public RankedPage(String key, ArrayList<VotingPage> voters) {
-    }
+public class RankedPage implements Serializable {
+    String voter;
+    double rank;
+    ArrayList<VotingPage> voterList = new ArrayList<>();
     
+    public RankedPage(String voter,double rank, ArrayList<VotingPage> voters){
+        this.voter = voter;
+        this.voterList = voters;
+        this.rank = rank;
+    }    
+    public RankedPage(String voter, ArrayList<VotingPage> voters){
+        this.voter = voter;
+        this.voterList = voters;
+        this.rank = 1.0;
+    }    
+    
+    public String getVoter() {
+        return voter;
+    }
+
+    public void setVoter(String voter) {
+        this.voter = voter;
+    }
+
+    public ArrayList<VotingPage> getVoterList() {
+        return voterList;
+    }
+
+    public void setVoterList(ArrayList<VotingPage> voterList) {
+        this.voterList = voterList;
+    }
+
+    @Override
+    public String toString(){
+        return this.voter +"<"+ this.rank +","+ voterList +">";
+    }
+
+    public double getRank() {
+        return this.rank;
+    }
 }
